3f9b6071d5897a6688d3c2ed20f99dc38355ed1d
xiyuan1223 <1446327445@qq.com>
2019-12-22 19:37:56 +0800

    登录注册调通数据传输

--- springboot-mybatis-dubbo-zookeeper/customer/src/main/java/com/senior_web/customer/controller/UserController.java
+++ springboot-mybatis-dubbo-zookeeper/customer/src/main/java/com/senior_web/customer/controller/UserController.java
@@ -3,14 +3,17 @@ package com.senior_web.customer.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.senior_web.common.domin.User;
 import com.senior_web.common.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.json.JSONObject;
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
+@RequestMapping("/lr")
 public class UserController {
     @Resource
     @Reference(version = "1.0.0")
@@ -23,6 +26,44 @@ public class UserController {
         return userService.findUser();
     }
 
+    @RequestMapping(value = "/login",method = RequestMethod.POST,produces="application/json;charset=UTF-8")
+    @ResponseBody
+    public String login(@RequestBody Map<String,Object> map) throws JSONException {
+        System.out.println(map.get("username"));
+        System.out.println(map.get("password"));
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("status","1");
+
+        return jsonObject.toString();
+
+    }
+    @RequestMapping(value = "/register",method = RequestMethod.POST,produces="application/json;charset=UTF-8")
+    @ResponseBody
+    public Object register(@RequestBody Map<String,Object> map) throws JSONException {
+        System.out.println(map.get("username"));
+        System.out.println(map.get("password"));
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("status","2");
+        return jsonObject.toString();
+
+    }
+
+    @RequestMapping(value = "/tj",method = RequestMethod.POST,produces="application/json;charset=UTF-8")
+    @ResponseBody
+    public Object testJson(@RequestBody Map<String,Object> map) throws JSONException {
+        System.out.println(map.get("username"));
+        System.out.println(map.get("password"));
+        System.out.println(map.get("openid"));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("a","a");
+        jsonObject.put("tttt","tttt");
+        jsonObject.put("bbbbb","bbbbb");
+        System.out.println(jsonObject.getString("a"));
+        return jsonObject.toString();
+
+    }
     @GetMapping("getUserById")
     public User getUserById(){
         System.out.println("进来了！！！！！！！！！！！！！！！！");
